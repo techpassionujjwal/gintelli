9dbffd3b003f95b7945292f47a9ec30c5c97a251
ujjwal aws <ujjwal_55@rediffmail.com>
2019-10-22 15:22:23 +0000

    Added a new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld
     //     // Prints "Hello, World" to the terminal window. 
              public static void main(String args[]) 
                  { 
-                        System.out.println("Hello, World"); 
+                        System.out.println("Hello, World");
+                        System.out.println("Welcome to Git"); 
                 } 
  } 
